1b5b5c432315add2bc7205fdf61faa061d5400a9
dientrinh <dientrinhsy82@gmail.com>
2026-03-17 21:20:02 +0700

    Auto-remove processed SQL files

--- db/sql/insert_with_backup_ok.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
--- Step 1: Backup current data
-CREATE TABLE products_backup_16032026 AS
-SELECT * FROM products;
--- Step 2: Apply new change
-INSERT INTO products (product_name, is_active) 
-VALUES ('Mouse', 'Y');
-COMMIT;
